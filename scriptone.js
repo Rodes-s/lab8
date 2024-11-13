a03f4f523414f782987f11126fdcc722a46f1d21
Rodes-s <rms98@njit.edu>
2024-11-13 13:39:57 -0500

    lab8

---
+++ scriptone.js
@@ -0,0 +1,161 @@
+//question 1//
+let Name = "Rodes";    // Replace "John" with your name
+let age = 18;         // Replace 30 with your age
+
+console.log(Name ="Name");
+console.log(age = "Age");
+
+
+//question 2//
+let name = "Rodes";         
+let AGe = 18;              
+let isStudent = true;       
+let address;                
+
+console.log(script = "Name:");           
+console.log(number = "AGe:");              
+console.log(Boolean = "Is Student:"); 
+console.log( address = "Address:");  
+
+//question 3//
+let RollNumber = 15;
+
+if (RollNumber > 10) {
+    console.log("The RollNumber is greater than 10");
+}
+
+//question 4//
+let isMember = true;
+if(isMember == true)
+{
+    console.log("member discount applied");
+}
+
+//question 5// 
+let startvalue = 50;
+
+if (startvalue > 0){
+    console.log("the value is +");
+}
+
+else if(startvalue < 0){
+    console.log("the value is -");
+}
+
+else(startvalue ==0);{
+    console.log("the value is 0");
+}
+
+//question 6//
+let a = 10
+let b = 20
+
+if ("a ==b:", a == b){
+    console.log("# = to");
+}
+
+else if("a != b:", a != b){
+    console.log("# Not equal to");
+}
+
+else if("a > b:", a > b){
+    console.log("# greater than");
+}
+
+else if("a < b:", a < b){
+    console.log("# Less than");
+}
+
+else if("a >= b:", a >= b){
+    console.log(" Greater than or equal to");
+}
+
+else("a <= b:", a <= b);{
+    console.log("# Less than or equal to");
+}
+
+//question 7//
+let first_name = Rodes
+let last_name = Safe
+let Age = 18
+
+console.log("Hello, Rodes Safe, you are 18 years old.");
+
+//question 8//
+let num = 50;
+
+if (num > 0 && num < 100) {
+    console.log("The number is within range.");
+} 
+else {
+    console.log("The number is out of range.");
+}
+
+//question 9//
+let hasCar = true;
+let hasLicense = true;
+let hasInsurance = true;
+
+if (hasCar && hasLicense && hasInsurance) {
+    console.log("You can drive legally.");
+} 
+else {
+    console.log("Check your driving eligibility.");
+}
+
+//question 10//
+let Num = 21;
+
+if (Num % 2 === 0) {
+    console.log("The number is even.");
+} 
+else {
+    console.log("The number is odd.");
+}
+
+//question 11//
+let score = 85;
+
+if (score >= 90) {
+    console.log("Grade A");
+} 
+else if (score >= 80 && score < 90) {
+    console.log("Grade B");
+} 
+else {
+    console.log("Grade C");
+}
+
+
+
+/*if (number > 90){
+    house = "grade A";
+    console.log(house);
+}
+
+else if (number > 80){
+    house = "grade B"
+    console.log(house);
+}
+
+else if (number > 70){
+    house = "grade C"
+    console.log(house);
+}
+
+else{
+    house = "fail"
+    console.log(house)
+}
+
+number > 90 ? "grade A" : "grade B"*/
+
+
+
+
+
+
+
+
+
+
